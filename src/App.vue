9d6613b570a300a0ab26b6b9064abd4e1bf11942
yuqi <piecesy7@gmail.com>
2023-01-11 14:13:18 +0800

    create the UI

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+  // 改变盒子模型计算方式
+  box-sizing: border-box;
+}
+body{
+  font:bold 20px "Courier"
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  lintOnSave: false,
+  // rules: [
+    // {
+    //   test: /\.less$/i,
+    //   use: [
+    //     // compiles Less to CSS
+    //     "style-loader",
+    //     "css-loader",
+    //     "less-loader",
+    //   ],
+    // },
+  // ],
+})
